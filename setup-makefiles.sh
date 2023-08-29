fe20452f581670dd838721eebc2e4c0d1bc4ab86
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 15:58:31 -0500

    vili: Add vendor soong imports to extract
    Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -5,6 +5,26 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"device/xiaomi/sm8350-common",
+		"hardware/qcom-caf/common/libqti-perfd-client",
+		"hardware/qcom-caf/sm8350",
+		"hardware/qcom-caf/wlan",
+		"hardware/xiaomi",
+		"vendor/qcom/opensource/commonsys/display",
+		"vendor/qcom/opensource/commonsys-intf/display",
+		"vendor/qcom/opensource/dataservices",
+		"vendor/qcom/opensource/display",
+EOF
+}
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
 set -e
 
 # Required!
