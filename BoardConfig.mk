f4ba8360460fc0518f08f64e3283d5341dbdb610
Alexander Winkowski <dereference23@outlook.com>
2024-01-08 07:05:38 +0000

    vili: Add SKU props from stock
    Change-Id: Ib7b6a0d8b992f264762c21f477c42f2114e51c7f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,5 +40,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_QTI_VIBRATOR_EFFECT_LIB := libqtivibratoreffect.xiaomi
 TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM := true
 
+# Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+
 # Include proprietary files
 include vendor/xiaomi/vili/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -32,11 +32,19 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_idle_timer_ms=6000 \
     ro.surface_flinger.set_touch_timer_ms=6000 \
     ro.surface_flinger.set_display_power_timer_ms=1000
+
+
+# Sku properties
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/sku/,$(TARGET_COPY_OUT_ODM)/etc)
+
+
 
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/vili/vili-vendor.mk)
